0616258d21690bbc2e6d0f52824d2bb6862d9b98
Steven <steven@viddadigital.com>
2022-08-29 15:41:01 +0200

    Dynamically load podcasts from feed

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,51 +1,3 @@
-const playIconContainer = document.getElementById('play-icon');
-const audio = document.getElementById('audio-1');
-const progressBar = document.getElementById('seek-slider');
-const currentTime = document.getElementById('current-time');
-const durationTime = document.getElementById('duration');
-
-let playState = 'play';
-
-playIconContainer.addEventListener('click', () => {
-    if(playState === 'play') {
-        audio.play();
-        playState = 'pause';
-        playIconContainer.innerText = "Pause";
-    } else {
-        audio.pause();
-        playState = 'play';
-        playIconContainer.innerText = "Play";
-
-    }
-});
-
-function progressValue() {
-    progressBar.max = audio.duration;
-    progressBar.value = audio.currentTime;
-
-    currentTime.textContent = formatTime(audio.currentTime);
-    durationTime.textContent = formatTime(audio.duration);
-}
-
-setInterval(progressValue, 500);
-
-function formatTime(sec) {
-    let minutes = Math.floor(sec / 60);
-    let seconds = Math.floor(sec - minutes * 60);
-    if (isNaN(minutes)) minutes = 0
-    if (isNaN(seconds)) seconds = 0
-    if (seconds < 10) {
-        seconds = `0${seconds}`;
-    }
-    return `${minutes}:${seconds}`;
-}
-
-function changeProgressBar() {
-    audio.currentTime = progressBar.value;
-}
-
-progressBar.addEventListener("click", changeProgressBar);
-
 async function get_podcasts_from_feed() {
     let res = await fetch('https://stevenkoerts.nl/Een-goed-gesprek-met/feed.rss')
         .then(response => response.text())
@@ -73,7 +25,33 @@ async function get_podcasts_from_feed() {
 
 }
 
+const playStateMap = new Map();
+
+
+function formatTime(sec) {
+    let minutes = Math.floor(sec / 60);
+    let seconds = Math.floor(sec - minutes * 60);
+    if (isNaN(minutes)) minutes = 0
+    if (isNaN(seconds)) seconds = 0
+    if (seconds < 10) {
+        seconds = `0${seconds}`;
+    }
+    return `${minutes}:${seconds}`;
+}
+
+
+function truncate_title(title) {
+    let parts = title.split('-')
+    if (parts.length > 1) {
+        return [parts[0], parts[1]].join('-')
+    }
+    return parts[0]
+}
+
+
 function mkPodcastMarkup(file, title, id) {
+    playStateMap.set(id, 'play')
+
     let li = document.createElement('li');
     let div = document.createElement('div');
     let audio = document.createElement('audio');
@@ -86,26 +64,43 @@ function mkPodcastMarkup(file, title, id) {
 
     audio.id = `audio-${id}`;
     audio.src = file;
-    audio.preload="metadata";
+    audio.preload = "metadata";
     audio.loop = true;
 
-    p.innerText = title;
+    p.innerText = truncate_title(title);
 
     button.className = 'play-icon';
     button.id = `play-icon-${id}`;
+    button.innerText = "Play"
+    button.addEventListener('click', () => {
+        const playState = playStateMap.get(id)
+
+        if (playState === 'play') {
+            audio.play();
+            playStateMap.set(id, 'pause')
+            button.innerText = "Pause";
+        } else {
+            audio.pause();
+            playStateMap.set(id, 'play')
+            button.innerText = "Play";
+        }
+    })
 
     span_current_time.id = `current-time-${id}`;
     span_current_time.className = 'time';
     span_current_time.innerText = "0:00";
 
-    input_slider.id = 'seek-slider';
-    input_slider.type = 'range'; 
-    input_slider.max = "100"; 
-    input_slider.min = "0"; 
+    input_slider.id = `seek-slider-${id}`;
+    input_slider.type = 'range';
+    input_slider.max = "100";
+    input_slider.min = "0";
     input_slider.value = '0';
+    input_slider.addEventListener('click', () => {
+        audio.currentTime = input_slider.value;
+    })
 
     span_duration.id = `duration-${id}`;
-    span_duration.className = 'time'; 
+    span_duration.className = 'time';
     span_duration.innerText = '0:00';
 
 
@@ -117,11 +112,25 @@ function mkPodcastMarkup(file, title, id) {
     div.appendChild(span_current_time);
     div.appendChild(input_slider);
     div.appendChild(span_duration);
+    li.appendChild(div)
 
+    function progressValue() {
+        input_slider.max = audio.duration;
+        input_slider.value = audio.currentTime;
+
+        span_current_time.textContent = formatTime(audio.currentTime);
+        span_duration.textContent = formatTime(audio.duration);
+    }
+
+    setInterval(progressValue, 500);
+
+    return li
 }
 
 
 window.addEventListener('load', async () => {
     let p = await get_podcasts_from_feed();
-    console.log(p);
+    p.forEach((item, index) => {
+        document.getElementById("podcast-container").appendChild(mkPodcastMarkup(item.file, item.title, index))
+    })
 })
